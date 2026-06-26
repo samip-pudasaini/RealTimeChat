ad205e956ed94f547497b7337fe2f871a23c4e23
Samip Pudasaini <samip.pudasaini03@gmail.com>
2026-06-27 02:13:14 +1000

    Debugging for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ app.use("/api/message", messageRoutes);
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
-    app.get ("*", (req, res) => {
+    app.get ("*splat", (req, res) => {
         res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
     })
 }
